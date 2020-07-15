cacd6dda613cac63cc7c3ebc90de751f0d983fb9
Raghu Kaippully <rkaippully@gmail.com>
2020-07-15 12:21:22 +0530

    Update to latest dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Mozilla Public License v2.0"
             :url  "https://www.mozilla.org/en-US/MPL/2.0/"}
   :dependencies [[instaparse "1.4.10"]
-                 [com.fasterxml.jackson.core/jackson-core "2.10.0.pr1"]]
+                 [com.fasterxml.jackson.core/jackson-core "2.11.1"]]
   :plugins [[lein-cloverage "1.0.13"]
             [lein-shell "0.5.0"]
             [lein-ancient "0.6.15"]
@@ -14,7 +14,7 @@
   :profiles {:dev   [:clj10]
              :clj08 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :clj09 {:dependencies [[org.clojure/clojure "1.9.0"]]}
-             :clj10 {:dependencies [[org.clojure/clojure "1.10.0"]]}}
+             :clj10 {:dependencies [[org.clojure/clojure "1.10.1"]]}}
   :deploy-repositories [["releases" {:url   "https://clojars.org/repo"
                                      :creds :gpg}]]
   :aliases {"update-readme-version" ["shell" "sed" "-i" "" "s/org.clojars.rkaippully\\\\/scim-patch \"[0-9.]*\"/org.clojars.rkaippully\\\\/scim-patch \"${:version}\"/" "README.md"]}
